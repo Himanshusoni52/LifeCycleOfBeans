52e1206af5511138e96ab2c230069199838d2029
himanshu soni <49919683+Himanshusoni52@users.noreply.github.com>
2023-04-13 17:50:35 +0530

    update

--- src/main/java/com/springboot/Beans/FirstBeans.java
+++ src/main/java/com/springboot/Beans/FirstBeans.java
@@ -3,8 +3,8 @@ package com.springboot.Beans;
 public class FirstBeans {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String name ="Himanshu"; 
+		int EmpId = 250;
 	}
 
 }
